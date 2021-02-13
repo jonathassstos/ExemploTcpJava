b143d06055ca757cf6aba52c59e83eda3763acf1
Ubuntu <ubuntu@ip-172-31-39-192.ec2.internal>
2021-02-13 23:01:57 +0000

    ExemploTCP com comentários

--- src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPClient.java
+++ src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPClient.java
@@ -9,17 +9,24 @@ public class TCPClient{
     public static void main(String[] args){
         try {
             System.out.print("[ Conectando com o Servidor TCP    ..................  ");
-            Socket sock = new Socket("127.0.0.1", 3300);
+            Socket sock = new Socket("127.0.0.1", 3300); // cria o terminal de comunicação e pede a conexão na porta 
             System.out.println("[OK] ]");
             
             InputStream is = sock.getInputStream(); // Canal de entrada de dados
             OutputStream os = sock.getOutputStream(); // Canal de saída de dados
             String msg = "Olá, DCOMP!!!";
-            byte[] buf = msg.getBytes(); // Obtendo a respresntação em bytes da mensagem
+            byte[] buf = msg.getBytes(); // Obtendo a respresentação em bytes da mensagem
 
             System.out.print("[ Enviando mensagem    ..............................  ");
-            os.write(buf);
+            os.write(buf); // write aqui representa o Send
             System.out.println("[OK] ]");
+            
+            byte[] bresp = new byte[20]; // cria vetor de bytes para capturar a resposta
+            is.read(bresp); // read aqui representa o receive  (é bloqueante)
+            
+            String resp = new String(bresp); // Converte os bytes de resposta em string
+            System.out.println(resp);
+            
         }catch(Exception e){System.out.println(e);}    
         System.out.println("[ FIM ]");
     }
--- src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPServer.java
+++ src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPServer.java
@@ -10,24 +10,29 @@ public class TCPServer{
         
         try {
             System.out.print("[ Iniciando Servidor TCP    .........................  ");
-            ServerSocket ss = new ServerSocket(3300, 5, InetAddress.getByName("127.0.0.1"));
+            ServerSocket ss = new ServerSocket(3300, 5, InetAddress.getByName("127.0.0.1")); //Cria socket associado a uma porta, 
+            // define o tamanho da fila (5), ip no qual o socket está associado
             System.out.println("[OK] ]");
             
             System.out.print("[ Aquardando pedidos de conexão    ..................  ");
-            Socket sock = ss.accept(); // Operação bloqueante (aguardando pedido de conexão)
+            Socket sock = ss.accept(); // Operação bloqueante (aguarda pedido de conexão) que pega o primeiro da fila
             System.out.println("[OK] ]");
             
-            InputStream is = sock.getInputStream(); //Canal de entrada de dados
-            OutputStream os = sock.getOutputStream(); //Canal de saída de dados
-            byte[] buf = new byte[20]; // buffer de recepção
+            InputStream is = sock.getInputStream(); //Canal de entrada de dados (onde vai receber os dados)
+            OutputStream os = sock.getOutputStream(); //Canal de saída de dados (por onde vai enviar os dados)
+            byte[] buf = new byte[20]; // buffer de recepção (pedaço q queremos pegar do fluxo de entrada)
 
             System.out.print("[ Aguardando recebimento de mensagem   ..............  ");
-            is.read(buf); // Operação bloqueante (aguardando chegada de dados)
+            is.read(buf); // Operação bloqueante que representa o receive aqui (aguardando chegada de dados) (passagem de parametro por referencia)
             System.out.println("[OK] ]");
             
             String msg = new String(buf); // Mapeando vetor de bytes recebido para String
             
             System.out.println("  Mensagem recebida: "+ msg);
+            
+            String resp = "Tudo bem?";
+            os.write(resp.getBytes()); // write aqui funcionando como send pegando os bytes da resp e enviando
+            
         }catch(Exception e){System.out.println(e);}    
         System.out.println("[ FIM ]");
     }
